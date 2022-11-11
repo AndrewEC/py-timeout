97e416a3c86d12013e1e77421b9f401f18b0ad85
unknown <andrew.e.cumming@gmail.com>
2022-11-11 17:40:23 -0500

    Bumped to the latest version of the build utils, updated build script

--- build.py
+++ build.py
@@ -1,36 +1,31 @@
 import click
 from buildutils import BuildConfiguration
 from buildutils.plugins import CoveragePlugin, FlakePlugin,\
-    GenericCommandPlugin, GenericCleanPlugin, EnsureVenvActivePlugin, with_alias, group_plugins
+    GenericCommandPlugin, GenericCleanPlugin, EnsureVenvActivePlugin, group
 
 
 @click.command()
+@click.option('--profile', '-pr')
 @click.option('--plugins', '-p')
 @click.option('--list-plugins', '-l', is_flag=True)
-def main(plugins: str, list_plugins: bool):
-    plugin_names = plugins.split(',') if plugins is not None else []
-
-    build_config = (
+def main(profile: str, plugins: str, list_plugins: bool):
+    (
         BuildConfiguration()
         .config('build.ini')
         .plugins(
             EnsureVenvActivePlugin(),
-            with_alias('clean', GenericCleanPlugin('CLEAN', 'Remove previous build files.')),
-            with_alias('install', GenericCommandPlugin('INSTALL', 'Install required dependencies from requirements.txt file.')),
+            GenericCleanPlugin('CLEAN', 'Remove previous build files.'),
+            GenericCommandPlugin('INSTALL', 'Install required dependencies from requirements.txt file.'),
             FlakePlugin(),
             CoveragePlugin(),
-            group_plugins(
+            group(
                 'generate-docs',
                 GenericCommandPlugin('PREPARE_DOCS', 'Prepare Sphinx for generating documentation from inline comments.'),
                 GenericCommandPlugin('GENERATE_DOCS', 'Generate documentation from inline comments using Sphinx')
             )
         )
+        .build(profile, plugins, list_plugins)
     )
-
-    if list_plugins:
-        return build_config.print_available_plugins()
-
-    build_config.build(plugin_names)
 
 
 if __name__ == '__main__':
