b5338b7a4fd09890cf0a58192ef30ed75e0c36b9
unknown <andrew.e.cumming@gmail.com>
2023-11-06 12:01:41 -0500

    Improved createvenv script.

--- CreateVenv.ps1
+++ CreateVenv.ps1
@@ -1,13 +1,16 @@
 $CurrentFolder = Get-Location | Split-Path -Leaf
 $EnvFolder = "$CurrentFolder-venv"
 
+Write-Host "Using virtual environment directory of $EnvFolder"
 if (Test-Path $EnvFolder) {
+    Write-Host "Virtual environment directory already exists. Activating virtual environment."
     Invoke-Expression "./$EnvFolder/Scripts/Activate.ps1"
 } else {
+    Write-Host "Creating virtual environment at directory $EnvFolder"
     python -m venv $EnvFolder `
         && Invoke-Expression "./$EnvFolder/Scripts/Activate.ps1" `
-        && pip install -r requirements.txt
-
+        && pip install -r requirements.txt `
+    
     python setup.py install `
     	&& cd py-build-utils && python setup.py install && cd ..
 }
